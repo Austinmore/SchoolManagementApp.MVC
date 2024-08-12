9d355878a707293ec3e47fe86a158bfbd4a55d65
Austin Chiagozie <austinmore16@gmail.com>
2024-08-12 17:38:26 +0100

    school

--- obj/Debug/net8.0/SchoolManagementApp.MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchoolManagementApp.MVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48a3ee93f43734bbf9bce1238797bce9d516e11d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3089a857319e551e1163b88a1aaee127122913b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementApp.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
